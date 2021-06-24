4bf296e368e1946c87c1dc50b98dd58248351ac1
vishal sharma <vishal.sharma@fluper.in>
2021-06-24 12:23:35 +0530

    update tracker

--- src/components/statewisedata/statewise.js
+++ src/components/statewisedata/statewise.js
@@ -1,9 +1,11 @@
-import react, { useEffect } from 'react'
+import react, { useEffect, useState } from 'react'
  
 const Statewise = () => {
+    const [data, setData] = useState([])
     const getCovidData = async () =>{
         const res = await fetch ('https://api.covid19india.org/data.json')
         const actualData = await res.json()
+        setData (actualData.statewise)
     }
     useEffect(()=>{
         getCovidData()
@@ -21,14 +23,29 @@ const Statewise = () => {
                     <table className = "table table-hover">
                         <thead className = "thead-dark">
                             <tr>
-                            <th>State</th>
-                            <th>Confirmed</th>
-                            <th>Recovered</th>
-                            <th>Death</th>
-                            <th>Active</th>
-                            <th>Updated</th>
+                                <th>State</th>
+                                <th>Confirmed</th>
+                                <th>Recovered</th>
+                                <th>Death</th>
+                                <th>Active</th>
+                                <th>Updated</th>
                             </tr>
                         </thead>
+                        <tbody>
+                            {data.map((currElem, ind)=>{
+                                return(
+                                    <tr key = {ind}>
+                                        <td>{currElem.state}</td>
+                                        <td>{currElem.confirmed}</td>
+                                        <td>{currElem.recovered}</td>
+                                        <td>{currElem.deaths}</td>
+                                        <td>{currElem.active}</td>
+                                        <td>{currElem.lastupdatedtime}</td>
+                                    </tr>
+                                )
+                            })}
+                           
+                        </tbody>
                     </table>
                 </div>
             </div>
